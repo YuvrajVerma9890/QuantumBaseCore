eb64c1bf8ea423fab3499d597e08eabda83cb73f
YuvrajVerma9890 <99275935+YuvrajVerma9890@users.noreply.github.com>
2022-04-10 20:26:29 +0530

    Add files via upload

---
+++ qb-electrician/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+description 'Electrician Job - By Spinks'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_script 'client/main.lua'
+server_script 'server/main.lua'
